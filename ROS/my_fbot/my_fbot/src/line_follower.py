3b2bf83c24a6809b983173e66f1ec473dc04c24e
jorge <jorge.andre.silva.fernandes@gmail.com>
2021-12-30 12:26:42 +0000

    update

--- ROS/my_fbot/my_fbot/src/line_follower.py
+++ ROS/my_fbot/my_fbot/src/line_follower.py
@@ -11,11 +11,45 @@ bridge = CvBridge()
 cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 twist = Twist()
 
+# dictionary with ranges
+ranges_pcss = {"b": {"min": 100, "max": 256},
+               "g": {"min": 100, "max": 256},
+               "r": {"min": 100, "max": 256},
+               }
+
+
+def onTrackbar(threshold):
+    pass
+
 
 def img_callback(data):
     try:
         img = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # # read the values on the trackbars
+        # min_b_pcss = cv2.getTrackbarPos("min B", "hsv")
+        # max_b_pcss = cv2.getTrackbarPos("max B", "hsv")
+        # min_g_pcss = cv2.getTrackbarPos("min G", "hsv")
+        # max_g_pcss = cv2.getTrackbarPos("max G", "hsv")
+        # min_r_pcss = cv2.getTrackbarPos("min R", "hsv")
+        # max_r_pcss = cv2.getTrackbarPos("max R", "hsv")
+        #
+        # # attribute the read values to the ranges dictionary
+        # ranges_pcss["b"]["min"] = min_b_pcss
+        # ranges_pcss["b"]["max"] = max_b_pcss
+        # ranges_pcss["g"]["min"] = min_g_pcss
+        # ranges_pcss["g"]["max"] = max_g_pcss
+        # ranges_pcss["r"]["min"] = min_r_pcss
+        # ranges_pcss["r"]["max"] = max_r_pcss
+
+        # # numpy arrays
+        # lower_yellow = np.array([ranges_pcss['b']['min'], ranges_pcss['g']['min'], ranges_pcss['r']['min']])
+        # upper_yellow = np.array([ranges_pcss['b']['max'], ranges_pcss['g']['max'], ranges_pcss['r']['max']])
+
+        # transform the image and show it
+        # mask = cv2.inRange(img, mins_pcss, maxs_pcss)  # colors mask
+        # img = cv2.bitwise_and(img, img, mask=mask)
 
         # change below lines to map the color you wanted robot to follow
         lower_yellow = np.array([10, 10, 10])
@@ -43,9 +77,12 @@ def img_callback(data):
             # print(err)
             twist.linear.x = 0.2
             twist.angular.z = -float(err) / 100
+            # if err > 80 or err < -80:
+            #     twist.linear.x = 0.05
             cmd_vel_pub.publish(twist)
 
         cv2.imshow("mask", mask)
+        cv2.imshow("hsv", hsv)
         cv2.imshow("image", img)
 
         k = cv2.waitKey(1) & 0xFF
@@ -55,9 +92,20 @@ def img_callback(data):
 
 
 def main():
-    rospy.init_node('line_follower')
-    img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, img_callback)
-    rospy.spin()
+    # cv2.namedWindow("hsv")
+    #
+    # # create the trackbars
+    # cv2.createTrackbar("min B", "hsv", 10, 255, onTrackbar)
+    # cv2.createTrackbar("max B", "hsv", 255, 255, onTrackbar)
+    # cv2.createTrackbar("min G", "hsv", 10, 255, onTrackbar)
+    # cv2.createTrackbar("max G", "hsv", 255, 255, onTrackbar)
+    # cv2.createTrackbar("min R", "hsv", 10, 255, onTrackbar)
+    # cv2.createTrackbar("max R", "hsv", 250, 255, onTrackbar)
+
+    while not rospy.is_shutdown():
+        rospy.init_node('line_follower')
+        img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, img_callback)
+        rospy.spin()
 
 
 if __name__ == "__main__":
