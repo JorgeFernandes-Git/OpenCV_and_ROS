6c8ce2ef0b7c00dbfa9fc8726b8a2a00b1726792
jorge <jorge.andre.silva.fernandes@gmail.com>
2022-01-05 21:38:18 +0000

    update

--- ROS/my_fbot/my_fbot/src/lidar_scan.py
+++ ROS/my_fbot/my_fbot/src/lidar_scan.py
@@ -1,88 +1,57 @@
 #!/usr/bin/python3
 
-from functools import partial
 import rospy
 from sensor_msgs.msg import LaserScan
-from visualization_msgs.msg import MarkerArray, Marker
-from geometry_msgs.msg import Point
-import math
-import random
+from geometry_msgs.msg import Point, Twist
+
+cmd_vel_node = rospy.remap_name("/cmd_vel")
+cmd_vel_pub = rospy.Publisher(cmd_vel_node, Twist, queue_size=10)
+twist = Twist()
+
+"""NOTES:
+
+lidar range: 0 to 180 degrees
+720 values in total
+4 values per degree
+msg.ranges[360] position in front of the robot
+"""
 
 
-###################
-# rosrun lidar_example lidar_subscriber.py
-# rostopic list
-# rostopic echo /left_laser/point_cloud --noarr
-###################
-
-
-# function to create marker
-# def create_marker():
-#     # Create marker
-#     marker = Marker()
-#     marker.header.frame_id = "left_laser"
-#     marker.header.stamp = rospy.Time.now()
-#     marker.ns = "my_namespace"
-#     marker.id = 0
-#     marker.type = Marker.SPHERE_LIST
-#     marker.pose.orientation.w = 1.0 # otherwise quaternion is not normalized
-#     marker.scale.x = 0.1
-#     marker.scale.y = 0.1
-#     marker.scale.z = 0.1
-#     marker.color.a = 0.5
-#     marker.color.r = random.random()
-#     marker.color.g = random.random()
-#     marker.color.b = random.random()
-#
-#     return marker
+def publisher(speed, turn):
+    twist.linear.x = speed
+    twist.angular.z = -float(turn) / 100
+    cmd_vel_pub.publish(twist)
 
 
 def callback_msg_received(msg):
-    # rospy.loginfo("Received laser scan message")
 
-    # marker_array = MarkerArray()
+    turn = 0
+    err_inc = 0.1
+    err_min = 0
+    err_max = 90
 
-    z = 0
-    for idx, range in enumerate(msg.ranges):
-        print(range)
+    speed = 0.5
+    spd_inc = 0.001
+    spd_min = 0
+    spd_max = 0.5
 
-        # if range < 0.1:  # 100 cm is the min distance so don't attach the point if tit's under
-        #     continue
+    obj_dist_min = 0.5
+    angle_min = 160
+    angle_max = 360
 
-        # print(range)
+    for range in msg.ranges:
+        if range < obj_dist_min:
+            speed = 0
+            turn = 90
 
-    #     # convert from polar to cartesian coordinates
-    #     theta = msg.angle_min + msg.angle_increment * idx
-    #     x = range * math.cos(theta)
-    #     y = range * math.sin(theta)
-    #
-    #     # Should I create a new cluster?
-    #     dist = math.sqrt((x_prev - x) ** 2 + (y_prev - y) ** 2)  # dist between points
-    #
-    #     if dist > dist_threshold:  # new cluster
-    #         marker = create_marker()
-    #         idx_group = len(marker_array.markers)
-    #         marker.id = idx_group  # give new id to marker
-    #
-    #         marker.points = []
-    #         marker_array.markers.append(marker)
-    #
-    #     last_marker = marker_array.markers[-1]  # last item of the list
-    #     last_marker.points.append(Point(x=x, y=y, z=z))
-    #
-    #     # print(last_marker)
-    #
-    #     # calculate the centroid
-    #     # cent_x = [p[0] for p in last_marker.pose.position.x]
-    #     # cent_y = [p[1] for p in marker]
-    #     # centroid = (sum(cent_x) / len(marker_array.markers), sum(cent_y) / len(marker_array.markers))
-    #     # print(centroid)
-    #
-    #     x_prev = x
-    #     y_prev = y
-    #
-    # publisher.publish(marker_array)
-    # rospy.loginfo('Published marker array')
+    # only cover a range
+    # for idx in range(angle_min, angle_max):
+    #     if msg.ranges[idx] < obj_dist_min:
+    #         speed = spd_min
+    #         turn = err_max
+
+    print(speed, turn)
+    publisher(speed=speed, turn=turn)
 
 
 def main():
