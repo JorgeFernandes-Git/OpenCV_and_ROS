a15153818e9d52978b016f2c6d97edbc57825328
jorge <jorge.andre.silva.fernandes@gmail.com>
2022-01-22 16:27:17 +0000

    update

--- ROS/p_jfernandes_pck/p_jfernandes_player/src/driver.py
+++ ROS/p_jfernandes_pck/p_jfernandes_player/src/driver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import copy
 import math
 
@@ -14,38 +15,52 @@ pose_stamped_node = rospy.remap_name("/move_base_simple/goal")
 
 class Driver:
     def __init__(self):
+
+        # define speed commands
         self.twist = Twist()
         self.speed = 0
         self.angle = 0
 
+        # define object for final position
         self.goal = PointStamped()
         self.goal_active = False
+        self.distance_to_goal = 0
 
+        # define object for converting to robot link
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
 
+        # define pubs and subs
         self.pub = rospy.Publisher(cmd_vel_node, Twist, queue_size=10)
         self.sub = rospy.Subscriber(pose_stamped_node, PoseStamped, self.goal_callback)
 
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.send_command_callback)
+        # define time to call function
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.send_command_callback)  # call function in each 0.1 seconds
 
     def goal_callback(self, goal_msg):
-        self.goal = goal_msg
-        self.goal_active = True
+        self.goal = goal_msg  # give goal x, y, z coordinates
+        self.goal_active = True  # goal received flag
 
     def send_command_callback(self, msg):
 
+        # if goal position is received
         if self.goal_active:
-            self.speed, self.angle = self.drive_straight(self.goal)
-        else:
+            self.speed, self.angle, self.distance_to_goal = self.drive_straight(self.goal)
+
+            # verify if goal achieved
+            if self.distance_to_goal < 0.1:
+                self.goal_active = False
+
+        else:  # if no goal stop
             self.speed = 0
             self.angle = 0
 
+        # publish a speed msg
         self.twist.linear.x = self.speed
         self.twist.angular.z = self.angle
         self.pub.publish(self.twist)
 
-    def drive_straight(self, goal, speed_min=0.1, speed_max=0.9):
+    def drive_straight(self, goal, speed_min=0.1, speed_max=1.0):
 
         # for a copy of the goal to keep goal in buffer and don't lose actual position over time
         goal_present_time = copy.deepcopy(goal)
@@ -65,14 +80,26 @@ class Driver:
 
         x = goal_in_base_link.pose.position.x
         y = goal_in_base_link.pose.position.y
+        orientation = goal_in_base_link.pose.orientation.z
 
-        self.angle = math.atan2(y, x)
-        self.speed = 2.5  # define speed over speed_max
+        print(orientation)
+
+        # calculate distance to goal
+        self.distance_to_goal = math.sqrt(x**2 + y**2)
+
+        # calculate angle and speed based on distance
+
+        if abs(orientation) > 0.5:
+            self.angle = math.atan2(y, x)
+            self.speed = 0
+        else:
+            self.angle = math.atan2(y, x)
+            self.speed = 0.5 * self.distance_to_goal
 
         self.speed = min(self.speed, speed_max)
         self.speed = max(self.speed, speed_min)
 
-        return self.speed, self.angle
+        return self.speed, self.angle, self.distance_to_goal
 
 
 def main():
