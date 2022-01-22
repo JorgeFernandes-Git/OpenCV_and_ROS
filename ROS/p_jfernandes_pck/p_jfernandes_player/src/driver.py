4c1340ed9be2fea60b9ae9ffd71437055fe006e8
jorge <jorge.andre.silva.fernandes@gmail.com>
2022-01-22 13:19:23 +0000

    update

---
+++ ROS/p_jfernandes_pck/p_jfernandes_player/src/driver.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import copy
+import math
+
+import rospy
+import tf2_ros
+from geometry_msgs.msg import PoseStamped, Twist
+
+# topics
+cmd_vel_node = rospy.remap_name("p_jfernandes/cmd_vel")
+pose_stamped_node = rospy.remap_name("/move_base_simple/goal")
+
+
+class Driver:
+    def __init__(self):
+        self.twist = Twist()
+        self.speed = 0
+        self.angle = 0
+
+        self.goal = PoseStamped()
+        self.goal_active = False
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
+
+        self.pub = rospy.Publisher(cmd_vel_node, Twist, queue_size=10)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.send_command_callback)
+        self.sub = rospy.Subscriber(pose_stamped_node, PoseStamped, self.goal_callback)
+
+    def goal_callback(self, goal_msg):
+        self.goal = goal_msg
+        self.goal_active = True
+
+    def send_command_callback(self, msg):
+
+        if self.goal_active:
+            self.speed, self.angle = self.drive_straight(self.goal)
+        else:
+            self.speed = 0
+            self.angle = 0
+
+        self.twist.linear.x = self.speed
+        self.twist.angular.z = self.angle
+        self.pub.publish(self.twist)
+
+    def drive_straight(self, goal, speed_min=0.1, speed_max=0.9):
+
+        goal_present_time = copy.deepcopy(goal)
+        goal_present_time.header.stamp = rospy.Time.now()
+
+        target_frame = "/p_jfernandes/base_link"
+        try:
+            goal_in_base_link = self.tf_buffer.transform(goal, target_frame, rospy.Duration(1))
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.logwarn(f'Could not transform goal from {goal.header.frame_id} to {target_frame}')
+            self.speed = 0
+            self.angle = 0
+            return self.speed, self.angle
+
+        x = goal_in_base_link.pose.position.x
+        y = goal_in_base_link.pose.position.y
+
+        self.angle = math.atan2(y, x)
+
+        self.speed = min(self.speed, speed_max)
+        self.speed = min(self.speed, speed_min)
+
+        return self.speed, self.angle
+
+
+def main():
+    rospy.init_node("move_to_goal")
+    driver = Driver()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
